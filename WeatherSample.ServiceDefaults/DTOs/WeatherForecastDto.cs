2a29990d7760b84349e0f541cecc4b75caa3e7ab
Jayanta Konjengbam <jayanta.konjengbam@emsentry-engineering.ca>
2025-04-30 13:32:37 -0400

    More updates to DTO
    1. Forgot to replace set by init in property
    2. Everything else remains the same.

--- WeatherSample.ServiceDefaults/DTOs/WeatherForecastDto.cs
+++ WeatherSample.ServiceDefaults/DTOs/WeatherForecastDto.cs
@@ -2,8 +2,8 @@ namespace WeatherSample.ServiceDefaults.DTOs;
 
 public record WeatherForecastDto
 {
-    public required DateOnly Date { get; set; }
-    public required int TemperatureC { get; set; }
-    public string? Summary { get; set; }
+    public required DateOnly Date { get; init; }
+    public required int TemperatureC { get; init; }
+    public string? Summary { get; init; }
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556); // Calculated property
 }
